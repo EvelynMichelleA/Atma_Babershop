d3c810fb0b97de8d5256c6eea2603cada6089190
EvelynMichelleA <michelleaurelia1544@gmail.com>
2024-03-26 17:09:10 +0700

    ok

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -8,7 +8,7 @@ export const anton = Anton({
   weight: ['400'],
   subsets: ['latin'],
 });
-export const lusitana = Kanit({
+export const lusitana = Lusitana({
   weight: ['400'],
   subsets: ['latin'],
 });
